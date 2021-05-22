3f74c3ccd69538d3227a977d1476ac4d4ffbdcbb
Luka Cabraja <luka@cabraja.eu>
2021-05-22 20:54:23 +0200

    added portrait filepath

--- DataHandler/Model/Matches.cs
+++ DataHandler/Model/Matches.cs
@@ -206,199 +206,202 @@ namespace DataHandler.Model
         public Position Position { get; set; }
         [JsonProperty(Required = Required.Default)]
         public bool Favorite { get; set; }
-    }
 
-    public partial class Weather
+        [JsonProperty(Required = Required.Default)]
+        public string PortraitPath {get; set;}
+}
+
+public partial class Weather
+{
+    [JsonProperty("humidity")]
+    [JsonConverter(typeof(ParseStringConverter))]
+    public long Humidity { get; set; }
+
+    [JsonProperty("temp_celsius")]
+    [JsonConverter(typeof(ParseStringConverter))]
+    public long TempCelsius { get; set; }
+
+    [JsonProperty("temp_farenheit")]
+    [JsonConverter(typeof(ParseStringConverter))]
+    public long TempFarenheit { get; set; }
+
+    [JsonProperty("wind_speed")]
+    [JsonConverter(typeof(ParseStringConverter))]
+    public long WindSpeed { get; set; }
+
+    [JsonProperty("description")]
+    public string Description { get; set; }
+}
+
+public enum TypeOfEvent { Goal, GoalOwn, GoalPenalty, SubstitutionIn, SubstitutionOut, YellowCard, YellowCardSecond, RedCard };
+
+public enum Position { Defender, Forward, Goalie, Midfield };
+
+internal static class Converter
+{
+    public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
     {
-        [JsonProperty("humidity")]
-        [JsonConverter(typeof(ParseStringConverter))]
-        public long Humidity { get; set; }
-
-        [JsonProperty("temp_celsius")]
-        [JsonConverter(typeof(ParseStringConverter))]
-        public long TempCelsius { get; set; }
-
-        [JsonProperty("temp_farenheit")]
-        [JsonConverter(typeof(ParseStringConverter))]
-        public long TempFarenheit { get; set; }
-
-        [JsonProperty("wind_speed")]
-        [JsonConverter(typeof(ParseStringConverter))]
-        public long WindSpeed { get; set; }
-
-        [JsonProperty("description")]
-        public string Description { get; set; }
-    }
-
-    public enum TypeOfEvent { Goal, GoalOwn, GoalPenalty, SubstitutionIn, SubstitutionOut, YellowCard, YellowCardSecond, RedCard };
-
-    public enum Position { Defender, Forward, Goalie, Midfield };
-
-    internal static class Converter
-    {
-        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
-        {
-            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
-            DateParseHandling = DateParseHandling.None,
-            Converters =
+        MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+        DateParseHandling = DateParseHandling.None,
+        Converters =
             {
                 TypeOfEventConverter.Singleton,
                 PositionConverter.Singleton,
                 new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
             },
-        };
+    };
+}
+
+internal class ParseStringConverter : JsonConverter
+{
+    public override bool CanConvert(Type t) => t == typeof(long) || t == typeof(long?);
+
+    public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+    {
+        if (reader.TokenType == JsonToken.Null) return null;
+        var value = serializer.Deserialize<string>(reader);
+        long l;
+        if (Int64.TryParse(value, out l))
+        {
+            return l;
+        }
+        throw new Exception("Cannot unmarshal type long");
     }
 
-    internal class ParseStringConverter : JsonConverter
+    public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
     {
-        public override bool CanConvert(Type t) => t == typeof(long) || t == typeof(long?);
-
-        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+        if (untypedValue == null)
         {
-            if (reader.TokenType == JsonToken.Null) return null;
-            var value = serializer.Deserialize<string>(reader);
-            long l;
-            if (Int64.TryParse(value, out l))
-            {
-                return l;
-            }
-            throw new Exception("Cannot unmarshal type long");
-        }
-
-        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
-        {
-            if (untypedValue == null)
-            {
-                serializer.Serialize(writer, null);
-                return;
-            }
-            var value = (long)untypedValue;
-            serializer.Serialize(writer, value.ToString());
+            serializer.Serialize(writer, null);
             return;
         }
-
-        public static readonly ParseStringConverter Singleton = new ParseStringConverter();
+        var value = (long)untypedValue;
+        serializer.Serialize(writer, value.ToString());
+        return;
     }
 
-    internal class TypeOfEventConverter : JsonConverter
+    public static readonly ParseStringConverter Singleton = new ParseStringConverter();
+}
+
+internal class TypeOfEventConverter : JsonConverter
+{
+    public override bool CanConvert(Type t) => t == typeof(TypeOfEvent) || t == typeof(TypeOfEvent?);
+
+    public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
     {
-        public override bool CanConvert(Type t) => t == typeof(TypeOfEvent) || t == typeof(TypeOfEvent?);
-
-        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+        if (reader.TokenType == JsonToken.Null) return null;
+        var value = serializer.Deserialize<string>(reader);
+        switch (value)
         {
-            if (reader.TokenType == JsonToken.Null) return null;
-            var value = serializer.Deserialize<string>(reader);
-            switch (value)
-            {
-                case "goal":
-                    return TypeOfEvent.Goal;
-                case "goal-own":
-                    return TypeOfEvent.GoalOwn;
-                case "goal-penalty":
-                    return TypeOfEvent.GoalPenalty;
-                case "substitution-in":
-                    return TypeOfEvent.SubstitutionIn;
-                case "substitution-out":
-                    return TypeOfEvent.SubstitutionOut;
-                case "yellow-card":
-                    return TypeOfEvent.YellowCard;
-                case "yellow-card-second":
-                    return TypeOfEvent.YellowCardSecond;
-                case "red-card":
-                    return TypeOfEvent.RedCard;
-            }
-            throw new Exception("Cannot unmarshal type TypeOfEvent");
+            case "goal":
+                return TypeOfEvent.Goal;
+            case "goal-own":
+                return TypeOfEvent.GoalOwn;
+            case "goal-penalty":
+                return TypeOfEvent.GoalPenalty;
+            case "substitution-in":
+                return TypeOfEvent.SubstitutionIn;
+            case "substitution-out":
+                return TypeOfEvent.SubstitutionOut;
+            case "yellow-card":
+                return TypeOfEvent.YellowCard;
+            case "yellow-card-second":
+                return TypeOfEvent.YellowCardSecond;
+            case "red-card":
+                return TypeOfEvent.RedCard;
         }
-
-        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
-        {
-            if (untypedValue == null)
-            {
-                serializer.Serialize(writer, null);
-                return;
-            }
-            var value = (TypeOfEvent)untypedValue;
-            switch (value)
-            {
-                case TypeOfEvent.Goal:
-                    serializer.Serialize(writer, "goal");
-                    return;
-                case TypeOfEvent.GoalOwn:
-                    serializer.Serialize(writer, "goal-own");
-                    return;
-                case TypeOfEvent.GoalPenalty:
-                    serializer.Serialize(writer, "goal-penalty");
-                    return;
-                case TypeOfEvent.SubstitutionIn:
-                    serializer.Serialize(writer, "substitution-in");
-                    return;
-                case TypeOfEvent.SubstitutionOut:
-                    serializer.Serialize(writer, "substitution-out");
-                    return;
-                case TypeOfEvent.YellowCard:
-                    serializer.Serialize(writer, "yellow-card");
-                    return;
-                case TypeOfEvent.YellowCardSecond:
-                    serializer.Serialize(writer, "yellow-card-second");
-                    return;
-                case TypeOfEvent.RedCard:
-                    serializer.Serialize(writer, "red-card");
-                    return;
-            }
-            throw new Exception("Cannot marshal type TypeOfEvent");
-        }
-
-        public static readonly TypeOfEventConverter Singleton = new TypeOfEventConverter();
+        throw new Exception("Cannot unmarshal type TypeOfEvent");
     }
 
-    internal class PositionConverter : JsonConverter
+    public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
     {
-        public override bool CanConvert(Type t) => t == typeof(Position) || t == typeof(Position?);
-
-        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+        if (untypedValue == null)
         {
-            if (reader.TokenType == JsonToken.Null) return null;
-            var value = serializer.Deserialize<string>(reader);
-            switch (value)
-            {
-                case "Defender":
-                    return Position.Defender;
-                case "Forward":
-                    return Position.Forward;
-                case "Goalie":
-                    return Position.Goalie;
-                case "Midfield":
-                    return Position.Midfield;
-            }
-            throw new Exception("Cannot unmarshal type Position");
+            serializer.Serialize(writer, null);
+            return;
         }
-
-        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
+        var value = (TypeOfEvent)untypedValue;
+        switch (value)
         {
-            if (untypedValue == null)
-            {
-                serializer.Serialize(writer, null);
+            case TypeOfEvent.Goal:
+                serializer.Serialize(writer, "goal");
                 return;
-            }
-            var value = (Position)untypedValue;
-            switch (value)
-            {
-                case Position.Defender:
-                    serializer.Serialize(writer, "Defender");
-                    return;
-                case Position.Forward:
-                    serializer.Serialize(writer, "Forward");
-                    return;
-                case Position.Goalie:
-                    serializer.Serialize(writer, "Goalie");
-                    return;
-                case Position.Midfield:
-                    serializer.Serialize(writer, "Midfield");
-                    return;
-            }
-            throw new Exception("Cannot marshal type Position");
+            case TypeOfEvent.GoalOwn:
+                serializer.Serialize(writer, "goal-own");
+                return;
+            case TypeOfEvent.GoalPenalty:
+                serializer.Serialize(writer, "goal-penalty");
+                return;
+            case TypeOfEvent.SubstitutionIn:
+                serializer.Serialize(writer, "substitution-in");
+                return;
+            case TypeOfEvent.SubstitutionOut:
+                serializer.Serialize(writer, "substitution-out");
+                return;
+            case TypeOfEvent.YellowCard:
+                serializer.Serialize(writer, "yellow-card");
+                return;
+            case TypeOfEvent.YellowCardSecond:
+                serializer.Serialize(writer, "yellow-card-second");
+                return;
+            case TypeOfEvent.RedCard:
+                serializer.Serialize(writer, "red-card");
+                return;
         }
-
-        public static readonly PositionConverter Singleton = new PositionConverter();
+        throw new Exception("Cannot marshal type TypeOfEvent");
     }
+
+    public static readonly TypeOfEventConverter Singleton = new TypeOfEventConverter();
+}
+
+internal class PositionConverter : JsonConverter
+{
+    public override bool CanConvert(Type t) => t == typeof(Position) || t == typeof(Position?);
+
+    public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+    {
+        if (reader.TokenType == JsonToken.Null) return null;
+        var value = serializer.Deserialize<string>(reader);
+        switch (value)
+        {
+            case "Defender":
+                return Position.Defender;
+            case "Forward":
+                return Position.Forward;
+            case "Goalie":
+                return Position.Goalie;
+            case "Midfield":
+                return Position.Midfield;
+        }
+        throw new Exception("Cannot unmarshal type Position");
+    }
+
+    public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
+    {
+        if (untypedValue == null)
+        {
+            serializer.Serialize(writer, null);
+            return;
+        }
+        var value = (Position)untypedValue;
+        switch (value)
+        {
+            case Position.Defender:
+                serializer.Serialize(writer, "Defender");
+                return;
+            case Position.Forward:
+                serializer.Serialize(writer, "Forward");
+                return;
+            case Position.Goalie:
+                serializer.Serialize(writer, "Goalie");
+                return;
+            case Position.Midfield:
+                serializer.Serialize(writer, "Midfield");
+                return;
+        }
+        throw new Exception("Cannot marshal type Position");
+    }
+
+    public static readonly PositionConverter Singleton = new PositionConverter();
+}
 }
