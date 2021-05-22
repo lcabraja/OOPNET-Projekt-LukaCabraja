f0b0230c2e7540095a76b937946ce654d72536eb
Luka Cabraja <luka@cabraja.eu>
2021-05-22 17:15:06 +0200

    added new form for displaying ranked lists

---
+++ WinFormsInterface/Forms/RankedList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinFormsInterface
+{
+    public partial class RankedList : Form
+    {
+        public RankedList()
+        {
+            InitializeComponent();
+        }
+
+        private void RankedList_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
