4f93514fe2710f4c7d7d5a440b65098bd522ca76
Luka Cabraja <luka@cabraja.eu>
2021-05-23 13:27:05 +0200

    visual studio kept crashing when i tried to do localization the propper way so i gave up and made my own thing

---
+++ DataHandler/Localizer.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DataHandler
+{
+    public class Localizer
+    {
+        JObject locales;
+        string currentLocale;
+        public Localizer(string locale)
+        {
+            var jsonData = File.ReadAllText("locale.json");
+            locales = JObject.Parse(jsonData);
+            currentLocale = locale;
+        }
+        public string Resource(string request)
+        {
+            return Resource(request, currentLocale);
+        }
+        public string Resource(string request, string locale)
+        {
+            try
+            {
+                return locales[request][locale].ToString();
+            }
+            catch (Exception ex)
+            {
+                return "AAAAAAAAa";
+            }
+        }
+    }
+}
